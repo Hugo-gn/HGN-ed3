fca130289c88b6f2042e4ad8ee8e4db25b4baed2
Hugo Garcia Nieto <hugo.garcia.hgn@gmail.com>
2022-04-27 17:47:50 +0200

    modulo

---
+++ modulo.java
@@ -0,0 +1,26 @@
+public class modulo {
+
+    private String nombre;
+
+    private String contenido;
+
+    private int horas;
+
+    private Alumno[] alumnado;
+
+    private Profesor[] claustro;
+
+    /***
+     *Esta funcion sirve para matricular a los alumnos en el modulo. 
+     */
+    
+    public void matricular() {
+    }
+    
+    /**
+     *Esta funcion muestra la duracion del modulo
+     */
+    
+    public void duracion() {
+    }
+}
